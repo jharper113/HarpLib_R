d8a08d694dd6958baf3670233b73bab9d80c3f13
Jon Harper <jharper113@outlook.com>
2016-11-09 22:05:02 -0500

    Update Template.R
    update template with tidyverse steps

--- Template.R
+++ Template.R
@@ -1,62 +1,85 @@
 ###########################################################################
 
-## R Code Template
+#   R Code Template
 
-## Date
+#   Description of what was this script does
+#   Inputs
+#   Outputs
 
-
-## Description of what was this script does
-
-###########################################################################
-
-# Initialize Workspace
-suppressWarnings(try(rm(list=ls()),silent=TRUE))   #clears the workspace
-switch(Sys.info()[['sysname']],
-       Windows = {setwd("c:/Users/jharper/Dropbox/03_School/Programming/R/FileDirectory/")},
-       Linux = {stop("hold on, this is a Linux Box!")},
-       Darwin ={setwd("/Users/harper/Dropbox/03_School/Programming/R/FileDirectory/")}
-)
-require(quantstrat)
-source(file = "./Scripts/HarpStrats/HarpFunctions.R")
-
+#   Date
 
 ###########################################################################
+
+# Initialization ----------------------------------------------------------
+# suppressWarnings(try(rm(list=ls()),silent=TRUE)) # clear the workspace 
+# switch(Sys.info()[['sysname']],
+#       Windows = {setwd("c:/Users/jharper/Dropbox/03_School/Programming/R/FileDirectory/")},
+#       Linux = {stop("hold on, this is a Linux Box!")},
+#       Darwin ={setwd("/Users/harper/Dropbox/03_School/Programming/R/FileDirectory/")}
+# )
+library(tidyverse)
+# source(file = "./HarpFunctions.R")
+
+
 # Variables ---------------------------------------------------------------
-###########################################################################
+file.log    <- "logfile.txt"
+file.input  <- "input.file.txt"
+file.output <- "output.file.txt"
 var1 <- 1
 var2 <- 2
 var3 <- 3
 
 
-###########################################################################
 # Functions ---------------------------------------------------------------
-###########################################################################
-
-#  ------------------------------------------------------------------------
-SampleFunction <- function(arg1, arg2, arg3) {
+SampleFunction <- function(arg1, arg2, arg3, verbose = FALSE) {
   # Summary of Function
   #
-  # Args      
-  #   arg1         description and example, like "number, 1, 2, 3.1, etc"
-  #   arg2
-  #   arg3 
+  # Args:      
+  #   arg1: Description, like "One of two vectors whose sample covariance is to be calculated."
+  #   arg2: Description, like "The other vector. x and y must have the same length, greater than one,
+  #      with no missing values.
+  #   arg3: Description and example.
+  #   verbose: If TRUE, sends output to console or log file. default is False.
   #
-  # Returns 
-  #   creates a bunch of new folders and sets some variables 
-  # -----------------------------------------------------------------------
+  # Returns:
+  #   a data frame 
+  # Variables
+  # Error handling
+  if (verbose)
+    cat("Summary", round(covariance, 4), ".\n", sep = "", file = "file.log")
 }
 
 
-###########################################################################
 # Main --------------------------------------------------------------------
-###########################################################################
 
-## Print output to the console
+# Import data 
+x <- read_csv(file = file.input, 
+         n_max =  10 * 1000,
+         col_types = cols(col1 = col_double(), col2 = col_datetime())
+         )
+
+# Tidy data
+gather(data = x, key = col1, value = col2)
+spread(data = x, key = col1, value = col2)
+
+# Transform 
+x %>% 
+  filter(col1 > 3, col2 != 10) %>% 
+  select() %>% 
+  mutate() %>% 
+  arrange() %>% 
+  summarise() %>% 
+  group_by() %>%
+  
+# Visualize  
+  ggplot2() + 
+    geom_point(mapping = aes(x = col1, y = col2))
+
+# Print output to the console
 cat("\n\n-------------------------\n")
 cat("Print output to the console\n\n")
 cat(date())
 cat("\n")
 
-## List files in the directory
-files.in.dir <- list.files(path = input.dir, pattern = "*")
-
+# Save output
+ggsave()
